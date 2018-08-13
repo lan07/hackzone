adba8f6d27b42f27a010102f0367fad56a1c8276
lan07 <42335140+lan07@users.noreply.github.com>
2018-08-13 13:31:27 +0530

    Create price.py

---
+++ price.py
@@ -0,0 +1,16 @@
+for d in json_price_data:
+            price.append({'name': d['name'], 'price': d['amazon_price'], 'url': d['amazon_url']})
+            price.append({'name': d['name'], 'price': d['walmart_price'], 'url': d['walmart_url']})
+            price.append({'name': d['name'], 'price': d['ebay_price'], 'url': d['ebay_url']})
+            minPricedItem = min(price, key=lambda x: float(x['price']))
+            store_name = ''
+            # Pick the store name based on url
+            if 'amazon' in minPricedItem['url'].lower():
+                store_name = 'Amazon'
+            elif 'walmart' in minPricedItem['url'].lower():
+                store_name = 'Amazon'
+            elif 'ebay' in minPricedItem['url'].lower():
+                store_name = 'eBay'
+            print('{} is available in cheap price at {}. The price is ${}'.format(minPricedItem['name'], store_name,
+                                                                                 minPricedItem['price']))
+            price = []
